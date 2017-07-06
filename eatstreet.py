74d8e1004eb3e8645a22bc447462c67fa41c03f6
rhartung <robyn@hartung.com>
2017-07-06 04:42:07 +0000

    - Added new function to eatstreet.py - get cuisine count returns a dictionary with keys of cuisine types and values are count of restaurants that deliver to user's address and are listed under cuisine type key

--- eatstreet.py
+++ eatstreet.py
@@ -51,3 +51,49 @@ def get_restaurant_list(search_result):
         restaurant_list.append((name, full_address))
 
     return restaurant_list
+
+
+def get_cuisine_count(address):
+    """Searches eat street API for all restaurants that deliver to user.
+
+       Args: address is user's full address
+
+       Returns dict: keys are cuisine types
+                     values are how many restaurants in users delivery area
+                     that are listed under that cuisine type."""
+
+    # headers contain authentication information
+    headers = {"X-Access-Token": os.environ["EAT_ACCESS_TOKEN"]}
+
+    params = {"street-address": address, "method": "delivery"}
+
+    # get restaurants that meet search terms from Eat Street
+    response = requests.get(
+        "https://api.eatstreet.com/publicapi/v1/restaurant/search?",
+        params=params,
+        headers=headers)
+
+    result = response.json()
+
+    restaurants = result["restaurants"]
+
+    cuisine_list = []
+
+    for restaurant in restaurants:
+        food_types = restaurant['foodTypes']
+
+        for cuisine in food_types:
+            cuisine_list.append(cuisine)
+
+    cuisine_count = {}
+
+    for cuisine in cuisine_list:
+        first_word = cuisine.split(' ')[0]
+
+        if first_word not in cuisine_count:
+            cuisine_count[first_word] = 1
+
+        else:
+            cuisine_count[first_word] += 1
+
+    return cuisine_count
